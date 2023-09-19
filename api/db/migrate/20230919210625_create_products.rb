2a8a101a8f840da42caf0a7b011e722c82d55280
treva556 <marktreva92@gmail.com>
2023-09-20 00:07:35 +0300

    model

---
+++ api/db/migrate/20230919210625_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :price
+      t.text :description
+      t.string :image
+      t.string :location
+      t.references :seller, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
