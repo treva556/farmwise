abd8c32b9cc5f1094e1e4c9ecd149ceb68545d6c
treva556 <marktreva92@gmail.com>
2023-09-23 18:05:07 +0300

    showcase

--- client/src/components/ItemShowcase.js
+++ client/src/components/ItemShowcase.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Itemcard from "./Itemcard";
 
+// Define a custom style for smaller item cards
+const smallerItemCardStyle = {
+  width: "200px", // Adjust the width as needed
+  height: "250px", // Adjust the height as needed
+};
+
 function ItemShowcase() {
   return (
     <div className="min-h-screen bg-yellow-300">
-      <div>
+      <div className="py-4">
         <ul className="flex items-center space-x-6 pl-4">
           <li>Collection</li>
           <li>Phones</li>
@@ -13,13 +19,23 @@ function ItemShowcase() {
           <li>Glasses</li>
         </ul>
       </div>
+      <h1 className="text-3xl font-bold">Item Showcase</h1>
+      <h2 className="text-xl">Explore our products</h2>
 
-      <div className="container mx-auto py-8">
-        <h1 className="text-3xl font-bold">Item Showcase</h1>
-        <h2 className="text-xl">Explore our products</h2>
-
-        {/* Render your Itemcard component here */}
-        <Itemcard />
+      {/* Render your Itemcard components with smallerItemCardStyle */}
+      <div className="container mx-auto py-8 flex flex-wrap justify-center space-x-4 space-y-4">
+        <div style={smallerItemCardStyle} className="mr-4">
+          <Itemcard />
+        </div>
+        <div style={smallerItemCardStyle} className="mr-4">
+          <Itemcard />
+        </div>
+        <div style={smallerItemCardStyle} className="mr-4">
+          <Itemcard />
+        </div>
+        <div style={smallerItemCardStyle} className="mr-4">
+          <Itemcard />
+        </div>
       </div>
     </div>
   );
