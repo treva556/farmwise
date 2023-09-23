2d3ebb163426adca813425b1da146d570bb528e2
treva556 <marktreva92@gmail.com>
2023-09-23 03:08:08 +0300

    home start

---
+++ client/src/components/ItemShowcase.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ItemShowcase () {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+  //
+  export default ItemShowcase;
+  
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Ad from "../components/Advert";
+import Footer from "../components/footer";
+import Searchbar from "../components/Searchbar";
+import ItemShowcase from "../components/ItemShowcase";
 
 function Home() {
     return (
       <div>
-        lllllllllllllll
+        <header>
+         <Ad/>
+         <Searchbar/>
+        </header>
+        <body>
+          <ItemShowcase/>
+        </body>
+        <Footer/>
       </div>
     );
   }
