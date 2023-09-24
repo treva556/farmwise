ed41c311c9f496c2f4a23a9ad38b336cde221955
treva556 <marktreva92@gmail.com>
2023-09-24 20:30:27 +0300

    show

--- client/src/components/ItemShowcase.js
+++ client/src/components/ItemShowcase.js
@@ -6,23 +6,32 @@ import pic1 from "../images/pic1.png"; // Import the image
 
 const catNav = [
     {
-        name: "Mobiles",
+        name: "Farm Produce",
         icon: pic1, // Make  you import and declare your icons
     },
     {
-        name: "Fashion",
+        name: "Animal Produce",
         icon: pic1,
     },
+    {
+      name: "Farming Equipment",
+      icon: pic1,
+  },
+  {
+      name: "Services",
+      icon: pic1,
+  },
+
     // ... (other categories)
 ]
 
 const ItemShowcase = () => {
     return (
-        <section className="hidden sm:block bg-white mt-10 mb-4 min-w-full px-12 py-1 shadow overflow-hidden">
+        <section className="hidden sm:block bg-white mt-10 mb-4 min-w-full px-9 py-1 shadow overflow-hidden">
             <div className="flex items-center justify-between mt-4">
                 {catNav.map((item, i) => (
                     <Link to={`/products?category=${item.name}`} className="flex flex-col gap-1 items-center p-2 group" key={i}>
-                        <div className="h-16 w-16">
+                        <div className="h-20 w-44">
                             <img draggable="false" className="h-full w-full object-contain" src={item.icon} alt={item.name} />
                         </div>
                         <span className="text-sm text-gray-800 font-medium group-hover:text-primary-blue">{item.name}</span>
