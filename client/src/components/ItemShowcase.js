0105c1819c8dc0c8a6648376f1c47736a45ee99a
treva556 <marktreva92@gmail.com>
2023-09-25 02:45:52 +0300

    show

--- client/src/components/ItemShowcase.js
+++ client/src/components/ItemShowcase.js
@@ -1,68 +1,95 @@
-import React from "react";
-import Itemcard from "./Itemcard";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import pic1 from "../images/pic1.png"; // Import the image
-// import Slider from 'react-slick';
-
 
 const catNav = [
-    {
-        name: "Farm Produce",
-        icon: pic1, // Make  you import and declare your icons
-    },
-    {
-        name: "Animal Produce",
-        icon: pic1,
-    },
-  //   {
-  //     name: "Farming Equipment",
-  //     icon: pic1,
-  // },
-  // {
-  //     name: "Services",
-  //     icon: pic1,
-  // },
-
-    // ... (other categories)
-]
+  {
+    name: "Farm Produce",
+    subcategories: ["Cereals", "Fruits", "Vegetables"], // Define subcategories
+  },
+  {
+    name: "Animal Produce",
+    subcategories: ["Milk", "Eggs", "Meat"],
+  },
+  // Add more categories and subcategories as needed
+];
 
 const ItemShowcase = () => {
-    return (
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedSubcategory, setSelectedSubcategory] = useState(null);
 
-      <>
-                        <section className="bg-  w-full shadow overflow-hidden">
+  // Function to handle category click
+  const handleCategoryClick = (index) => {
+    setSelectedCategory(index);
+    setSelectedSubcategory(null); // Reset subcategory when a new category is clicked
+  };
 
-          <div className="flex items-center justify-between mt-4">
-                {catNav.map((item, i) => (
-                    <Link to={`/products?category=${item.name}`} className="flex flex-col gap-1 items-center p-2 group" key={i}>
-                        <div className="h-20 w-44">
-                            <img draggable="false" className="h-full w-full object-contain" src={item.icon} alt={item.name} />
-                        </div>
-                        <span className="text-sm text-gray-800 font-medium group-hover:text-primary-blue">{item.name}</span>
-                    </Link>
-                ))}
+  // Function to handle subcategory click
+  const handleSubcategoryClick = (subcategoryIndex) => {
+    setSelectedSubcategory(subcategoryIndex);
+  };
+
+  return (
+    <section className="bg-white w-full shadow overflow-hidden">
+      <div className="flex px-6 py-3 justify-between items-center">
+        <Link to="/products" className="bg-primary-blue text-xs font-medium text-white px-5 py-2.5 rounded-sm shadow-lg">VIEW ALL</Link>
+      </div>
+      <hr />
+
+      {/* Category cards */}
+      <div className="flex justify-center flex-col items-center space-y-4 mt-4">
+        {catNav.map((item, i) => (
+          <div key={i}>
+            <img
+              draggable="false"
+              className={`h-20 w-20 object-contain ${
+                selectedCategory === i ? "filter invert" : ""
+              }`}
+              src={pic1}
+              alt={item.name}
+              onClick={() => handleCategoryClick(i)}
+            />
+            <div
+              className={`cursor-pointer p-2 group ${
+                selectedCategory === i ? "bg-primary-blue text-white" : "bg-white text-gray-800"
+              }`}
+              onClick={() => handleCategoryClick(i)}
+            >
+              {item.name}
             </div>
-        </section>
-      
-                {/* <section className="bg-white w-full shadow overflow-hidden">
-            {/* <!-- header --> */}
-            {/* <div className="flex px-6 py-3 justify-between items-center">
-                {/* <h1 className="text-xl font-medium">{name}</h1> */}
-                {/* <Link to="/products" className="bg-primary-blue text-xs font-medium text-white px-5 py-2.5 rounded-sm shadow-lg">VIEW ALL</Link>
+          </div>
+        ))}
+      </div>
+
+      {/* Subcategories */}
+      {selectedCategory !== null && (
+        <div className="flex justify-center space-x-4 mt-4">
+          {catNav[selectedCategory].subcategories.map((subcategory, subcategoryIndex) => (
+            <div
+              key={subcategoryIndex}
+              className={`cursor-pointer p-2 group ${
+                selectedSubcategory === subcategoryIndex ? "bg-primary-blue text-white" : "bg-white text-gray-800"
+              }`}
+              onClick={() => handleSubcategoryClick(subcategoryIndex)}
+            >
+              {subcategory}
             </div>
-            <hr />  */}
-            {/* <!-- header --> */}
+          ))}
+        </div>
+      )}
 
-                {/* <Slider {...settings}>
-                    {getRandomProducts(offerProducts, 12).map((item, i) => (
-                        <Product {...item} key={i} />
-                    ))}
-                </Slider> */} 
-
-        {/* </section> */}
-        </>
-    );
+      {/* Product display */}
+      {selectedSubcategory !== null && (
+        // Render products related to the selected subcategory here
+        <div className="mt-4">
+          {/* You can map through and display products here */}
+          <div>Product 1</div>
+          <div>Product 2</div>
+          {/* Add more product components as needed */}
+        </div>
+      )}
+    </section>
+  );
 };
-
 
 export default ItemShowcase;
