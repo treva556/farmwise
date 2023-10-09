83b9ab85aa9f4568e2bd4cb61531ff2d389d17b2
treva556 <marktreva92@gmail.com>
2023-10-10 00:00:29 +0300

    show

--- client/src/Seller/reg2.js
+++ client/src/Seller/reg2.js
@@ -1,32 +1,34 @@
 
-
 import React, { useRef } from 'react';
 import axios from 'axios';
 
 function Form() {
-
-    const nameRef = useRef()
-    const emailRef = useRef()
-    const phone_numberRef = useRef()
-    const locationRef = useRef()
-    const passwordRef = useRef()
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const phone_numberRef = useRef();
+  const locationRef = useRef();
+  const passwordRef = useRef();
     
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
         
-      const form_data = {
-        name: nameRef.current.value,
-        email: emailRef.current.value,
-        phone: phone_numberRef.current.value,
-        location: locationRef.current.value,
-        password: passwordRef.current.value
-      }
+    const form_data = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      phone: phone_numberRef.current.value,
+      location: locationRef.current.value,
+      password: passwordRef.current.value
+    };
 
-      console.log(form_data);
-
-      const res = await axios.post("http://localhost:3000", form_data);
-
+    try {
+      const res = await axios.post("http://localhost:3000/register", form_data);
+      console.log('Response from server:', res.data);
+      // Handle success, e.g., redirect or show a success message
+    } catch (error) {
+      console.error('Error from server:', error);
+      // Handle error, e.g., show an error message to the user
     }
+  };
 
   return (
     <div className="bg-yellow-400 flex justify-center items-center min-h-screen">
@@ -36,11 +38,10 @@ function Form() {
         <input type="text" ref={phone_numberRef} placeholder='Phone:'/>
         <input type="text" ref={locationRef} placeholder='Locat:'/>
         <input type="text" ref={passwordRef} placeholder='Password:'/>
-       <button type='submit'>Submit</button>
-
+        <button type='submit'>Submit</button>
       </form>
     </div>
   );
-};
+}
 
 export default Form;
