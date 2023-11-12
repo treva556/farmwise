e66a5816133889585d5cf37c10f2805c4cfd2642
treva556 <marktreva92@gmail.com>
2023-11-12 14:16:33 +0300

    Image progress

--- src/components/body.js
+++ src/components/body.js
@@ -1,6 +1,3 @@
-
-//body
-
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
@@ -21,7 +18,7 @@ function Bodyy() {
 
   const getImageUrl = (imageData) => {
     const defaultImageUrl = '/path/to/default/image.png';
-  
+
     if (imageData && imageData.image && imageData.image.url) {
       const imageUrl = `http://localhost:3000${imageData.image.url}`;
       console.log('Image URL:', imageUrl);
@@ -52,7 +49,6 @@ function Bodyy() {
         console.error('Error deleting category:', error);
       });
   };
-   
 
   return (
     <>
@@ -62,7 +58,11 @@ function Bodyy() {
           {categories.map((category) => (
             <div key={category.id} className="w-64 h-80 p-4 m-2 bg-yellow-200 rounded-lg shadow-md text-center hover:border-green-500 transition duration-300">
               <Link to={`/categories/${category.slug}/subcategories`} className="block mb-4">
-                <img src={getImageUrl(category.image)} alt={category.name} className="mx-auto h-32 w-32 object-contain mb-4" />
+                {category.image && category.image.url ? (
+                  <img src={`http://localhost:3000${category.image.url}`} alt={category.name} className="mx-auto h-32 w-32 object-contain mb-4" />
+                ) : (
+                  <div className="mx-auto h-32 w-32 bg-gray-300 mb-4"></div>
+                )}
                 <h2 className="font-bold text-xl">{category.name}</h2>
               </Link>
               {/* <button onClick={() => handleDelete(category.slug)}>Delete</button> Pass slug instead of id */}
@@ -75,5 +75,3 @@ function Bodyy() {
 }
 
 export default Bodyy;
-
-
