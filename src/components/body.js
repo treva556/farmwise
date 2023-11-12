6d36dc3f0e7b5d2c94059e87c3ba155f5d3f6d7a
treva556 <marktreva92@gmail.com>
2023-11-12 16:18:16 +0300

    fixing images

--- src/components/body.js
+++ src/components/body.js
@@ -1,3 +1,6 @@
+
+//code
+
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
@@ -16,19 +19,15 @@ function Bodyy() {
       });
   }, []);
 
-  const getImageUrl = (imageData) => {
-    const defaultImageUrl = '/path/to/default/image.png';
-
-    if (imageData && imageData.image && imageData.image.url) {
-      const imageUrl = `http://localhost:3000${imageData.image.url}`;
-      console.log('Image URL:', imageUrl);
-      return imageUrl;
-    } else if (typeof imageData === 'string' && imageData !== 'null') {
-      const imageUrl = `http://localhost:3000${imageData}`;
-      console.log('Image URL:', imageUrl);
+  const getImageUrl = (category) => {
+    const defaultImageUrl = 'http://localhost:3000/path/to/default/image.png';
+  
+    if (category.image_url) {
+      const imageUrl = `http://localhost:3000${category.image_url}`;
+      console.log('Image URL:', imageUrl); // Log the image URL
       return imageUrl;
     } else {
-      console.log('Using default image URL:', defaultImageUrl);
+      console.log('Using default image URL:', defaultImageUrl); // Log the default image URL
       return defaultImageUrl;
     }
   };
@@ -55,19 +54,20 @@ function Bodyy() {
       <div className="mt-8 bg-yellow-200">
         <h1 className="text-xl underline">Categories</h1>
         <div className="flex flex-wrap justify-around mt-8">
-          {categories.map((category) => (
-            <div key={category.id} className="w-64 h-80 p-4 m-2 bg-yellow-200 rounded-lg shadow-md text-center hover:border-green-500 transition duration-300">
-              <Link to={`/categories/${category.slug}/subcategories`} className="block mb-4">
-                {category.image && category.image.url ? (
-                  <img src={`http://localhost:3000${category.image.url}`} alt={category.name} className="mx-auto h-32 w-32 object-contain mb-4" />
-                ) : (
-                  <div className="mx-auto h-32 w-32 bg-gray-300 mb-4"></div>
-                )}
-                <h2 className="font-bold text-xl">{category.name}</h2>
-              </Link>
-              {/* <button onClick={() => handleDelete(category.slug)}>Delete</button> Pass slug instead of id */}
-            </div>
-          ))}
+        {categories.map((category) => (
+  <div key={category.id} className="w-64 h-80 p-4 m-2 bg-yellow-200 rounded-lg shadow-md text-center hover:border-green-500 transition duration-300">
+    <Link to={`/categories/${category.slug}/subcategories`} className="block mb-4">
+      {category.image_url ? (
+        <img src={getImageUrl(category)} alt={category.name} className="mx-auto h-32 w-32 object-contain mb-4" />
+      ) : (
+        <div className="mx-auto h-32 w-32 bg-gray-300 mb-4"></div>
+      )}
+      <h2 className="font-bold text-xl">{category.name}</h2>
+    </Link>
+    {/* <button onClick={() => handleDelete(category.slug)}>Delete</button> */}
+  </div>
+))}
+
         </div>
       </div>
     </>
