c07c6ddb0a755aca50b6d870bef2432760a4fa74
treva556 <marktreva92@gmail.com>
2023-11-24 17:56:31 +0300

    login

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,11 +1,11 @@
 
 
-
+// show products
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function ProductPage() {
-  const [product, setProduct] = useState(null);
+  const [products, setProducts] = useState(null);
   const { categorySlug, subcategorySlug, groupSlug } = useParams();
 
   useEffect(() => {
@@ -14,7 +14,7 @@ function ProductPage() {
       .then(data => {
         console.log('API Response:', data);
         if (data && data.length > 0) {
-          setProduct(data[0]);
+          setProducts(data); // Update state with all products
         }
       })
       .catch(error => {
@@ -22,27 +22,29 @@ function ProductPage() {
       });
   }, [categorySlug, subcategorySlug, groupSlug]);
 
-  if (!product) {
+  if (!products) {
     return <div>Loading...</div>;
   }
 
   return (
     <div className="bg-green-600 min-h-screen flex flex-col items-center p-8">
       <h1 className="text-3xl font-semibold text-white mb-8">Products</h1>
-      <div className="w-full max-w-3xl bg-yellow-300 p-8 rounded-lg shadow-lg">
-        <h1 className="text-2xl font-semibold mb-4">{product.name}</h1>
-        {/* Other product details */}
-    {/* Display all images */}
-{product.image_urls && product.image_urls.length > 0 && (
-  <div>
-    {product.image_urls.map((imageUrl, index) => (
-      <img key={index} src={imageUrl} alt={`Image ${index}`} className="max-w-full mb-4" />
-    ))}
-  </div>
-)}
-      </div>
+      {products.map(product => (
+        <div key={product.id} className="w-full max-w-3xl bg-yellow-300 p-8 rounded-lg shadow-lg mb-4">
+          <h1 className="text-2xl font-semibold mb-4">{product.name}</h1>
+          {product.image_urls && product.image_urls.length > 0 && (
+            <div>
+              {product.image_urls.map((imageUrl, index) => (
+                <img key={index} src={imageUrl} alt={`Image ${index}`} className="max-w-full mb-4" />
+              ))}
+            </div>
+          )}
+          {/* Other product details */}
+        </div>
+      ))}
     </div>
   );
 }
 
 export default ProductPage;
+
